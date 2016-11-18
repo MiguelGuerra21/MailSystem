b724b2a8ac00904b3805bd4536a168cdf5435389
Miguel Guerra <migueclase2@gmail.com>
2016-11-18 13:51:37 +0100

    añadido subject a mail item

--- MailItem.java
+++ MailItem.java
@@ -13,6 +13,8 @@ public class MailItem
     private String to;
     // The text of the message.
     private String message;
+    
+    private String subject;
 
     /**
      * Create a mail item from sender to the given recipient,
@@ -21,10 +23,11 @@ public class MailItem
      * @param to The intended recipient of this item.
      * @param message The text of the message to be sent.
      */
-    public MailItem(String from, String to, String message)
+    public MailItem(String from, String to,String subject, String message)
     {
         this.from = from;
         this.to = to;
+        this.subject = subject;
         this.message = message;
     }
 
@@ -51,7 +54,15 @@ public class MailItem
     {
         return message;
     }
-
+    
+    /**
+     * @return The subject of the message.
+     */
+    public String getAsunto()
+    {
+        return subject;
+    }
+    
     /**
      * Print this mail message to the text terminal.
      */
@@ -59,6 +70,7 @@ public class MailItem
     {
         System.out.println("From: " + from);
         System.out.println("To: " + to);
+        System.out.println("Subject: " + subject);
         System.out.println("Message: " + message);
     }
 }
